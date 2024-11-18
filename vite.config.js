00e4398843d506eb0eb8988cff99312dff136f1a
Meer Faridul Haque <meer@dirayaah.com>
2024-11-18 17:10:18 +0600

    resolving vite build error

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+		  external: ['ol/ol.css'], // Exclude the CSS from the build
+		},
+	  },
+	
 });
